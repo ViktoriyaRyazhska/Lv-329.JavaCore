0835a0fe0edab2b2d3e096b95b6aa3faec545b63
Oleg Leshko <Alegkalew@gmail.com>
2018-08-02 10:58:31 +0300

    complite practice_11

--- Practice_11/Main.java
+++ Practice_11/Main.java
@@ -1,16 +1,34 @@
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import static java.time.temporal.TemporalAdjusters.firstInMonth;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String args[]) {
         Random r = new Random();
-        r.ints(0, 10).limit(10).sorted().forEach(System.out::println);
+        r.ints(0, 9).limit(10).sorted().forEach(System.out::print);
+        System.out.println();
 
-        int[] arr =r.ints().limit(8).toArray();
-        System.out.println(Arrays.stream(arr).average());
-        Integer[] l =arr.f
-        List list= Arrays.asList(arr);
-        list.stream().mapToInt(x -> x).filter(x -> x >3).count();
+        int[] arr = r.ints(0, 9).limit(8).toArray();
+        Arrays.stream(arr).forEach(System.out::print);
+        System.out.println();
 
+        Integer[] arr1 = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+        Arrays.stream(arr1).forEach(System.out::print);
+        System.out.println();
+
+        List<Integer> list = Arrays.stream(arr).boxed().collect(Collectors.toList());
+        System.out.println(list.stream().filter(x -> x > 8).count());
+
+        IntSummaryStatistics statistics = list.stream().mapToInt(x -> x).summaryStatistics();
+        System.out.println("Max - " + statistics.getMax());
+
+        LocalDate localDate = LocalDate.now();
+        LocalDate localDate1 = LocalDate.now().with(firstInMonth(DayOfWeek.MONDAY));
+        System.out.println(localDate.getDayOfWeek());
+        System.out.println(localDate.getMonth());
+        System.out.println(localDate1);
     }
 }
