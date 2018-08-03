79e40bd32d0c4e730d65f782313a034f7afbb8e9
MakKeywa <mak.keywa@gmail.com>
2018-08-03 09:05:36 +0300

    add some code

---
+++ Company/src/com/mak_keywa/CompanyMain.java
@@ -0,0 +1,45 @@
+package com.mak_keywa;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class CompanyMain {
+	private static List<Stuff> company;
+	final static String progfile = "CProger.txt";
+	final static String cleafile = "Cleaner.txt";
+
+	public static void main(String[] args) {
+		company = new ArrayList<Stuff>();
+		Scanner sc = new Scanner(System.in);
+		company.add(new Programer("Vasya", 150, ProgerRank.Junior));
+		company.add(new Programer("Olga", 200, ProgerRank.Junior));
+		company.add(new Programer("Marta", 160, ProgerRank.Medium));
+		company.add(new Programer("Maria", 210, ProgerRank.Medium));
+		company.add(new Programer("Ostap", 170, ProgerRank.Senior));
+		company.add(new Programer());
+		company.get(5).input(sc);
+
+		company.add(new Cleaner("Nadya", 80));
+		company.add(new Cleaner("Nina", 100));
+		company.add(new Cleaner());
+
+		company.get(8).input(sc);
+		sc.close();
+
+		try {
+			company.get(2).input(progfile);
+			company.get(6).input(cleafile);
+		} catch (IOException e) {
+			System.err.println("File not found");
+		}
+
+		try {
+			Programer.print(progfile);
+			Cleaner.print(cleafile);
+		} catch (IOException e) {
+			System.err.println("File not found");
+		}
+	}
+}
---
+++ Company/src/com/mak_keywa/SomeExeption.java
@@ -0,0 +1,12 @@
+package com.mak_keywa;
+
+public class SomeExeption extends Exception {
+
+	public SomeExeption() {
+		super();
+	}
+
+	public SomeExeption(String message) {
+		super(message);
+	}
+}
