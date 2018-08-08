2c0e7a79d761b1ad549d3116d9a234613c9da043
MakKeywa <mak.keywa@gmail.com>
2018-08-08 10:13:37 +0300

    task5

--- Tasks/Main.java
+++ Tasks/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-	
+
 	public static void main(String args[]) {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Select task number");
@@ -18,9 +18,50 @@ public class Main {
 			Task3 t3 = new Task3();
 			t3.task3(s);
 			break;
-
-		default:
-
+		case 4:
+			// Task4 t4 = new Task4();
+			// t4.task4(s);
+			break;
+		case 5:
+			Task5 t5 = new Task5();
+			t5.task5(s);
+			break;
+		case 6:
+			// Task6 t6= new Task6();
+			// t6.task6(s);
+			break;
+		case 7:
+			// Task4 t4 = new Task4();
+			// t4.task4(s);
+			break;
+		case 8:
+			// Task4 t4 = new Task4();
+			// t4.task4(s);
+			break;
+		case 9:
+			// Task4 t4 = new Task4();
+			// t4.task4(s);
+			break;
+		case 10:
+			// Task4 t4 = new Task4();
+			// t4.task4(s);
+			break;
+		case 11:
+			// Task4 t4 = new Task4();
+			// t4.task4(s);
+			break;
+		case 12:
+			// Task4 t4 = new Task4();
+			// t4.task4(s);
+			break;
+		case 13:
+			// Task4 t4 = new Task4();
+			// t4.task4(s);
+			break;
+		case 14:
+			// Task4 t4 = new Task4();
+			// t4.task4(s);
+			break;
 		}
 	}
 }
---
+++ Tasks/Task5.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Task5 {
+	static String input;
+	static char a;
+	static char b;
+	static int j;
+	static boolean result = true;
+
+	public void task5(Scanner sc) {
+		System.out.println("Input word to check for palindrome:");
+		input = sc.next();
+		for (int i = 0; i < input.length() / 2; i++) {
+			j = input.length()-i-1;
+			a = input.charAt(i);
+			b = input.charAt(j);
+			if (a != b) {
+				result = false;
+			}
+
+		}
+		System.out.println("Result is " + result);
+	}
+
+}
