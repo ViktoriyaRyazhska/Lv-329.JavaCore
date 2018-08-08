5b19ca373ce4860b57711fb85a71d1eb2a632b2c
Oleg Leshko <Alegkalew@gmail.com>
2018-08-08 09:34:36 +0300

    add main

--- Tasks/Main.java
+++ Tasks/Main.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Select task number");
+        switch (s.nextInt()) {
+            case 1:
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+                break;
+            case 2:
 
-	}
+                break;
+            case 3:
 
+                break;
+
+            default:
+
+        }
+    }
 }
